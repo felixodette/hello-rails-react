3b5860ff175e3c516f17243738a42890294d61a3
Felix Odete <felix.odette@gmail.com>
2022-09-27 11:56:17 +0300

    Config: Greeting component

---
+++ app/javascript/Greeting/Greeting.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+
+const Greeting = () => {
+    async function getMessage() {
+        await fetch('api/messages')
+            .then((response) => {
+                response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            });
+    }
+    useEffect(() => {
+        getMessage()
+    }, [])
+    return <div>Greeting</div>
+};
+
+export default Greeting;
