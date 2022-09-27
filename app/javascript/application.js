173014982511e4c9e4a7831986bf5a700699bbb1
Felix Odete <felix.odette@gmail.com>
2022-09-27 11:59:54 +0300

    Config: React app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import ReactDom from 'react-dom';
+import Greeting from './Greeting/Greeting';
+
+function App() {
+    return (
+        <Greeting />
+    );
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
