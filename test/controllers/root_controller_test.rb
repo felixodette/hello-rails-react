1e2b344b28fb37af1746ddf217fa3da695435ac8
Felix Odete <felix.odette@gmail.com>
2022-09-27 12:21:48 +0300

    Config: Root controller test

--- test/controllers/root_controller_test.rb
+++ test/controllers/root_controller_test.rb
@@ -4,4 +4,8 @@ class RootControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'should get index' do
+    get root_index_url
+    assert_responses :success
+  end
 end
